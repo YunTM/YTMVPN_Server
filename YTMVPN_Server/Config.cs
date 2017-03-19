bfd209d92ce5c5aec8a8b0c9641ddcb985f22735
Lensual <lenvisual@gmail.com>
2017-03-20 01:23:18 +0800

    瞎写了点睡觉，upload to github

---
+++ YTMVPN_Server/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTMVPN_Server
+{
+    static class Config
+    {
+        public static string IP_Address;
+        public static UInt16 IP_AuthPort;
+        public static UInt16 IP_DataPort;
+
+    }
+}
--- YTMVPN_Server/Program.cs
+++ YTMVPN_Server/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace YTMVPN_Server
 {
@@ -6,7 +8,34 @@ namespace YTMVPN_Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //参数和配置文件的解析 先不写了，参数写死先
+            Config.IP_Address = "0.0.0.0";
+            Config.IP_AuthPort = 52145;
+
+            //认证Socket绑定
+            Socket authSocket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+            authSocket.Bind(new IPEndPoint(IPAddress.Parse(Config.IP_Address), Config.IP_AuthPort));
+            byte[] authBuffer = new byte[4096];
+            EndPoint authRemoteEP = new IPEndPoint(IPAddress.Any,0);
+
+            //接收认证消息
+            int count = authSocket.ReceiveFrom(authBuffer, ref authRemoteEP);
+
+
+
+
+            //数据Socket绑定
+            Socket dataSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            dataSocket.Bind(new IPEndPoint(IPAddress.Parse(Config.IP_Address), Config.IP_DataPort));
+            byte[] dataBuffer = new byte[4096];
+            EndPoint dataRemoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            //接收数据
+            dataSocket.ReceiveFrom(dataBuffer, ref dataRemoteEP);
+
+
+
         }
     }
+
 }
