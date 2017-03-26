510685ee612ecbf3d6ed471480dd497510413895
Lensual <lenvisual@gmail.com>
2017-03-26 23:45:50 +0800

    继续写DataPacket类

--- YTMVPN_Server/Packet/DataPacket.cs
+++ YTMVPN_Server/Packet/DataPacket.cs
@@ -7,10 +7,101 @@ namespace YTMVPN_Server.Packet
     //数据包的封装
     class DataPacket
     {
-        byte[] rawData; //这个包的raw数据
-        byte[] dstAddr; //目标地址
-        byte[] srcAddr; //源地址
-        byte[] payloadData; //载荷数据
+        //包的一些参数 必须指定
+        public int AddrLength { get; set; }
+        public int PortLength { get; set; }
+
+        //这个包的raw数据
+        private byte[] rawData;
+        byte[] RawData {
+            get { return rawData; }
+            set { rawData = value; }
+        }
+
+        //目标地址
+        byte[] DstAddr {
+            get {
+                byte[] dstAddr = new byte[AddrLength];
+                Buffer.BlockCopy(rawData, 0, dstAddr, 0, dstAddr.Length);
+                return dstAddr;
+            }
+            set {
+                for (int i = 0; i < AddrLength; i++)
+                {
+                    rawData[i] = value[i];
+                }
+            }
+        }
+
+        //源地址
+        byte[] SrcAddr
+        {
+            get
+            {
+                byte[] srcAddr = new byte[AddrLength];
+                Buffer.BlockCopy(rawData, AddrLength, srcAddr, 0, srcAddr.Length);
+                return srcAddr;
+            }
+            set
+            {
+                for (int i = 0; i < AddrLength; i++)
+                {
+                    rawData[i + AddrLength] = value[i];
+                }
+            }
+        }
+
+        //目标端口
+        byte[] DstPort
+        {
+            get
+            {
+                byte[] dstPort = new byte[PortLength];
+                Buffer.BlockCopy(rawData, AddrLength * 2, dstPort, 0, dstPort.Length);
+                return dstPort;
+            }
+            set
+            {
+                for (int i = 0; i < PortLength; i++)
+                {
+                    rawData[i + AddrLength * 2] = value[i];
+                }
+            }
+        }
+
+        //源端口
+        Byte[] SrcPort
+        {
+            get
+            {
+                byte[] srcPort = new byte[PortLength];
+                Buffer.BlockCopy(rawData, AddrLength * 2 + PortLength, srcPort, 0, srcPort.Length);
+                return srcPort;
+            }
+            set
+            {
+                for (int i = 0; i < PortLength; i++)
+                {
+                    rawData[i + AddrLength * 2 + PortLength] = value[i];
+                }
+            }
+        }
+
+        //载荷数据
+        byte[] PayloadData {
+            get {
+                byte[] payloadData = new byte[rawData.Length - AddrLength * 2 - PortLength * 2];
+                Buffer.BlockCopy(rawData, AddrLength * 2 + PortLength * 2 , payloadData, 0, payloadData.Length);
+                return payloadData;
+            }
+            set {
+                List<Byte> raw = new List<byte>();
+                raw.AddRange(raw);
+                raw.RemoveRange(AddrLength * 2 + PortLength * 2, raw.Count - AddrLength * 2 - PortLength * 2);
+                raw.AddRange(value);
+                rawData = raw.ToArray();
+            }
+        }
         
         
 
