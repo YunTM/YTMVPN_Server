65ad729afa7082ff55955724a6eeee0ed465e673
Lensual <lenvisual@gmail.com>
2017-03-29 13:16:46 +0800

    添加RoutingItem

---
+++ YTMVPN_Server/Service/Routing/RoutingItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using YTMVPN_Server.Packet;
+
+namespace YTMVPN_Server.Service.Routing
+{
+    class RoutingItem
+    {
+        public byte[] DstAddr { get; set; }
+        public byte[] DstPort { get; set; }
+        public ConcurrentQueue<DataPacket> OutputQueue { get; set; }
+        public RoutingItem(byte[] DstAddr, ConcurrentQueue<DataPacket> OutputQueue, byte[] DstPort = null)
+        {
+            this.DstAddr = DstAddr;
+            this.OutputQueue = OutputQueue;
+            this.DstPort = DstPort;
+        }
+    }
+}
---
+++ YTMVPN_Server/Service/Routing/RoutingTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTMVPN_Server.Service.Routing
+{
+    class RoutingTable
+    {
+
+    }
+}
