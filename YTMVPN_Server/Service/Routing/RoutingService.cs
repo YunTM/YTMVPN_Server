532e9aefc13114d54770d5f12580d005ba84f4b4
Lensual <lenvisual@gmail.com>
2017-03-29 11:14:59 +0800

    修改路由服务代码

--- YTMVPN_Server/Service/Routing/RoutingService.cs
+++ YTMVPN_Server/Service/Routing/RoutingService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using YTMVPN_Server.Packet;
 
 namespace YTMVPN_Server.Service.Routing
@@ -39,29 +40,23 @@ namespace YTMVPN_Server.Service.Routing
         }
         void DoRecv()
         {
-            //初始化局部变量
-            byte[] destAddr = new byte[Program.options.LogicAddrLength];
-            byte[] destPort = new byte[Program.options.LogicPortLength];
-
+            //大循环
             while (true)
             {
-                while (!RecvQueue.IsEmpty)
+                //循环直到队列为空，之后线程短暂休眠避免抢占cpu时间
+                while (!iQueue.IsEmpty)
                 {
-                    //初始化局部变量
                     //尝试从队列取出
-                    while (RecvQueue.TryDequeue(out RoutingPackge rp))
+                    if (iQueue.TryDequeue(out DataPacket dp))
                     {
-                        LogHelper.Log("RoutingService: Dequeue!");
-                        //取得目标地址和端口
-                        Buffer.BlockCopy(rp.Data, 0, destAddr, 0, Program.options.LogicAddrLength);  //地址
-                        Buffer.BlockCopy(rp.Data, Program.options.LogicAddrLength, destPort, 0, Program.options.LogicPortLength);  //端口
-
+                        LogHelper.Logging("RoutingService: Dequeue!");
+                        
                         //路由
-                        ConcurrentQueue<RoutingPackge> outQueue = RoutingTable.GetQueueByAddr(destAddr, destPort);  //!!!注意路由表线程安全
-                        if (outQueue != null)
+                        ConcurrentQueue<DataPacket> oQueue = RoutingTable.GetQueueByAddr(destAddr, destPort);  //!!!注意路由表线程安全
+                        if (oQueue != null)
                         {
                             //交给目标队列
-                            outQueue.Enqueue(rp);
+                            oQueue.Enqueue(dp);
                         }
                         else
                         {
@@ -69,8 +64,8 @@ namespace YTMVPN_Server.Service.Routing
                             //!!需要端口没有路由的情况
                             LogHelper.Log("DestAddr Unreachable: " + destAddr + ":" + destPort);  //!!地址格式化
                         }
-
                     }
+
                 }
                 //休眠
                 Thread.Sleep(10);
