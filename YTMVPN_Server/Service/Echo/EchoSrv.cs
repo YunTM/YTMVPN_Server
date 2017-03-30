89ecf0b8eee58b66f4451196c77434f87f8c4af5
Lensual <lenvisual@gmail.com>
2017-03-30 16:24:28 +0800

    为EchoSrv添加SrvPool

--- YTMVPN_Server/Service/Echo/EchoSrv.cs
+++ YTMVPN_Server/Service/Echo/EchoSrv.cs
@@ -10,6 +10,8 @@ namespace YTMVPN_Server.Service.Echo
 {
     public class EchoSrv : IService<DataPacket>
     {
+        public static List<EchoSrv> SrvPool { get; set; } = new List<EchoSrv>();
+
         #region Status
         private ESrvStatus status;
         public ESrvStatus Status { get { return status; } }
