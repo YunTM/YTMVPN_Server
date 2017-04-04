5ba54eaf45927039337c0f136537e853fbeca063
Lensual <lenvisual@gmail.com>
2017-04-04 23:56:50 +0800

    改用 Newtonsoft.Json

--- YTMVPN_Server/Config.cs
+++ YTMVPN_Server/Config.cs
@@ -1,19 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace YTMVPN_Server
 {
-    static class Config
+    public class Config
     {
-        public static string IP_Address;
-        public static UInt16 IP_AuthPort;
-        public static UInt16 IP_DataPort;
+        public string IP_Address;
+        public UInt16 IP_AuthPort;
+        public UInt16 IP_DataPort;
 
-        public static int Logic_AddrLength;
-        public static int Logic_PortLength;
-        public static byte[] Logic_LocalAddr;
-        
+        public int Logic_AddrLength;
+        public int Logic_PortLength;
+        public byte[] Logic_LocalAddr;
 
+        /// <summary>
+        /// 读取 config.json ，返回 Config 实例
+        /// </summary>
+        public static Config GetConfig(string filePath = "config.json")
+        {
+            return JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText(filePath, Encoding.UTF8));
+        }
     }
 }
